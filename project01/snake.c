32eeea4116eb2568db9b60538a5481fa30ff6863
Mactavish <maczhanchao@yahoo.com>
2022-08-27 16:39:58 +0200

    finish project01 task7

--- project01/snake.c
+++ project01/snake.c
@@ -30,23 +30,28 @@ int main(int argc, char* argv[]) {
 
   // Read board from file, or create default board if no input filename was given
   if (in_filename != NULL) {
-    // TODO: load the board from in_filename into state...
-    // TODO: ...then call initialize_snakes on the state you made
+    // load the board from in_filename into state...
+    // then call initialize_snakes on the state you made
+    state = load_board(in_filename);
+    initialize_snakes(state);
   } else {
-    // TODO: create the default state in state
+    // create the default state in state
+    state = create_default_state();
   }
 
-  // TODO: Update state. Use the deterministic_food function
+  // Update state. Use the deterministic_food function
   // (already implemented in state_utils.h) to add food.
-
+  // deterministic_food(state);
+  update_state(state, &deterministic_food);
   // Write updated board to file, or print to stdout if no output filename was given
   if (out_filename != NULL) {
-    // TODO: save the board to out_filename
+    // save the board to out_filename
+    save_board(state, out_filename);
   } else {
-    // TODO: print the board to stdout
+    print_board(state, stdout);
   }
 
-  // TODO: free any allocated memory
-
+  // free any allocated memory
+  free_state(state);
   return 0;
 }
