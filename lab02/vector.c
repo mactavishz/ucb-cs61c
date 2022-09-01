021c728114c219310784c49bde35cf109e9964c4
Mactavish <maczhanchao@yahoo.com>
2022-09-01 22:17:25 +0200

    finish lab02 exercise3

--- lab02/vector.c
+++ lab02/vector.c
@@ -1,6 +1,7 @@
 /* Include the system headers we need */
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 /* Include our header */
 #include "vector.h"
@@ -31,6 +32,9 @@ vector_t *bad_vector_new() {
         allocation_failed();
     }
 
+    // The vector is created on the stack, instead of the heap.
+    // All memory stored on the stack gets freed as soon as that function finishes runningso
+    // so when the function returns, we lose the vector we constructed.
     retval->data[0] = 0;
     return retval;
 }
@@ -47,41 +51,44 @@ vector_t also_bad_vector_new() {
         allocation_failed();
     }
     v.data[0] = 0;
+
+    // While this does work, it is rather inefficient.
+    // The vector object is a "Big" thing, so when we return it, we need to copy a lot of data from the return value to the calling frame
+    // Instead, it is generally better practice to store structs on the heap, and pass around pointers, as pointers are a fixed, "Small" size.
     return v;
 }
 
 /* Create a new vector with a size (length) of 1 and set its single component to zero... the
    right way */
-/* TODO: uncomment the code that is preceded by // */
 vector_t *vector_new() {
     /* Declare what this function will return */
-    // vector_t *retval;
+    vector_t *retval;
 
     /* First, we need to allocate memory on the heap for the struct */
-    // retval = /* YOUR CODE HERE */
+    retval = malloc(sizeof(vector_t));
 
     /* Check our return value to make sure we got memory */
-    // if (/* YOUR CODE HERE */) {
-    //     allocation_failed();
-    // }
+    if (retval == NULL) {
+        allocation_failed();
+    }
 
     /* Now we need to initialize our data.
        Since retval->data should be able to dynamically grow,
        what do you need to do? */
-    // retval->size = /* YOUR CODE HERE */;
-    // retval->data = /* YOUR CODE HERE */;
+    retval->size = 1;
+    retval->data = malloc(sizeof(int));
 
     /* Check the data attribute of our vector to make sure we got memory */
-    // if (/* YOUR CODE HERE */) {
-    //     free(retval);				//Why is this line necessary?
-    //     allocation_failed();
-    // }
+    if (retval->data == NULL) {
+        free(retval);				//Why is this line necessary?
+        allocation_failed();
+    }
 
     /* Complete the initialization by setting the single component to zero */
-    // /* YOUR CODE HERE */ = 0;
+    retval->data[0] = 0;
 
     /* and return... */
-    return NULL; /* UPDATE RETURN VALUE */
+    return retval;
 }
 
 /* Return the value at the specified location/component "loc" of the vector */
@@ -96,14 +103,18 @@ int vector_get(vector_t *v, size_t loc) {
     /* If the requested location is higher than we have allocated, return 0.
      * Otherwise, return what is in the passed location.
      */
-    /* YOUR CODE HERE */
-    return 0;
+    if (loc >= v->size) {
+      return 0;
+    } else {
+      return v->data[loc];
+    }
 }
 
 /* Free up the memory allocated for the passed vector.
    Remember, you need to free up ALL the memory that was allocated. */
 void vector_delete(vector_t *v) {
-    /* YOUR CODE HERE */
+  free(v->data);
+  free(v);
 }
 
 /* Set a value in the vector. If the extra memory allocation fails, call
@@ -112,6 +123,20 @@ void vector_set(vector_t *v, size_t loc, int value) {
     /* What do you need to do if the location is greater than the size we have
      * allocated?  Remember that unset locations should contain a value of 0.
      */
-
-    /* YOUR CODE HERE */
+    if (loc < v->size) {
+      v->data[loc] = value;
+    } else {
+      // calloc will set all values to 0
+      int *new_data = calloc(loc + 1, sizeof(int));
+      if (new_data == NULL) {
+        allocation_failed();
+      }
+      
+      // copy old data
+      memcpy(new_data, (const void *) v->data, v->size * sizeof(int));
+      free(v->data);
+      v->size = loc + 1;
+      v->data = new_data;
+      v->data[loc] = value;
+    }
 }
