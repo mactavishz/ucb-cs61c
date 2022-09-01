ec400cf7d6d5f9110ade5e0d3d47f18a1d7a9300
Mactavish <maczhanchao@yahoo.com>
2022-09-01 11:12:11 +0200

    finish lab02 exercise1

--- lab02/bit_ops.c
+++ lab02/bit_ops.c
@@ -3,17 +3,24 @@
 
 /* Returns the Nth bit of X. Assumes 0 <= N <= 31. */
 unsigned get_bit(unsigned x, unsigned n) {
-    /* YOUR CODE HERE */
-    return -1; /* UPDATE WITH THE CORRECT RETURN VALUE*/
+    unsigned mask = 1 <<n;
+    return (x & mask) >> n; /* UPDATE WITH THE CORRECT RETURN VALUE*/
 }
 
 /* Set the nth bit of the value of x to v. Assumes 0 <= N <= 31, and V is 0 or 1 */
 void set_bit(unsigned *x, unsigned n, unsigned v) {
-    /* YOUR CODE HERE */
+    unsigned mask_1 = ~(1 << n);
+    // set the nth bit to 0
+    *x &= mask_1;
+    // set the nth bit to v
+    unsigned mask_v = v << n;
+    *x |= mask_v;
 }
 
 /* Flips the Nth bit in X. Assumes 0 <= N <= 31.*/
 void flip_bit(unsigned *x, unsigned n) {
-    /* YOUR CODE HERE */
+  // just reuse the get_bit and set_bit functions
+  unsigned n_bit = get_bit(*x, n);
+  set_bit(x, n, ~n_bit & 1);
 }
 
